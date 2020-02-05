48d3b1404696827fd5373efd6635b4c610368f86
Alan K <akillian@outlook.com>
2020-02-05 14:23:40 -0500

    Another commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 // src/js/app.js
 
 /* jshint esversion:9 */
+
+const Utils = require("../resources/services/Utils");
+
 // List of supported routes. Any url other than these will throw a 404 error
 const routes = {
   "/": Home,
@@ -9,13 +12,22 @@ const routes = {
   "/register": Register
 };
 
-// The router code. takes a URL,checks against the list of supported routes and renders the corresponding content page.
+// The router code. Takes a URL, checks against the list of supported routes and renders the corresponding content page.
 const router = async () => {
   //Lazy Load view element:
+  const header = null || document.getElementById("header_container");
   const content = null || document.getElementById("page_container");
+  const footer = null || document.getElementById("footer.container");
+
+  // Render the Header and footer of the page
+  header.innerHTML = await Navbar.render();
+  await Navbar.after_render();
+  footer.innerHTML = await Bottombar.render();
+  await Bottombar.after_render();
 
   // Get the parsed URL from the addressbar
   let request = Utils.parseRequestURL();
+  console.log(request);
 
   // Parse the URL and if it has an id part, change it with the string ":id"
   let parsedURL =
@@ -30,7 +42,7 @@ const router = async () => {
   await page.after_render();
 }; //end router function
 
-// Listen oh hash change:
+// Listen on hash change:
 window.addEventListener("hashchange", router);
 
 // Listen on page load:
--- src/resources/services/Utils.js
+++ src/resources/services/Utils.js
@@ -1,15 +1,32 @@
+// src/resources/services/Utils.js
+/* jshint esversion:9 */
+
 const Utils = {
   // ========================================================
   // Parse a url and break it into resource, id and verb
   // =========================================================
 
   parseRequestURL: () => {
-    let url = location.hash.slice(1).toLocaleLowerCase() || "/";
-    let r = url.split("/");
+    let url = location.hash.slice(1).toLocaleLowerCase() || "/"; // returns hash portion of url sans the #
+    let r = url.split("/"); // Divide hash into component parts as an array
     let request = {
       resource: null,
       id: null,
       verb: null
     };
+    request.resource = r[1];
+    request.id = r[2];
+    request.verb = r[3];
+
+    return request;
+  },
+
+  // ================================
+  // Simple sleep implementation
+  // ================================
+  sleep: ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 };
+
+module.exports = Utils;
